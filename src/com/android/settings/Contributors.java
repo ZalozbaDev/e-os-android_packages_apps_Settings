41bfd88b437e3c8a4eef5dcdd6c8fb7326fb3c34
Romain Hunault <romain.hunault@eelo.io>
2018-12-20 15:57:39 +0100

    Add submenu for supporters

--- src/com/android/settings/Contributors.java
+++ src/com/android/settings/Contributors.java
@@ -12,8 +12,10 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 public class Contributors extends SettingsPreferenceFragment {
 
     private static final String LOG_TAG = "Contributors";
-    private static final String URL_E_CONTRIBUTORS = "https://e.foundation/support-us/#halloffame";
+    private static final String URL_E_CONTRIBUTORS = "https://e.foundation/about-e/#the-e-team";
     private static final String KEY_E_CONTRIBUTORS = "e_contributors";
+    private static final String URL_E_SUPPORTERS = "https://e.foundation/support-us/#halloffame";
+    private static final String KEY_E_SUPPORTERS = "e_supporters";
 
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -27,10 +29,18 @@ public class Contributors extends SettingsPreferenceFragment {
 
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
-        if (preference.getKey().equals(KEY_E_CONTRIBUTORS)) {
+        if (preference.getKey().equals(KEY_E_CONTRIBUTORS)
+          || preference.getKey().equals(KEY_E_SUPPORTERS) ) {
+
             final Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.addCategory(Intent.CATEGORY_DEFAULT);
-            intent.setData(Uri.parse(URL_E_CONTRIBUTORS));
+
+            if (preference.getKey().equals(KEY_E_CONTRIBUTORS)) {
+              intent.setData(Uri.parse(URL_E_CONTRIBUTORS));
+            } else if (preference.getKey().equals(KEY_E_SUPPORTERS)) {
+              intent.setData(Uri.parse(URL_E_SUPPORTERS));
+            }
+
             try {
                 startActivity(intent);
             } catch (Exception e) {
@@ -38,5 +48,24 @@ public class Contributors extends SettingsPreferenceFragment {
             }
         }
         return super.onPreferenceTreeClick(preference);
+            if (preference.getKey().equals(KEY_E_CONTRIBUTORS)
+              || preference.getKey().equals(KEY_E_SUPPORTERS) ) {
+
+                final Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+
+                if (preference.getKey().equals(KEY_E_CONTRIBUTORS)) {
+                  intent.setData(Uri.parse(URL_E_CONTRIBUTORS));
+                } else if (preference.getKey().equals(KEY_E_SUPPORTERS)) {
+                  intent.setData(Uri.parse(URL_E_SUPPORTERS));
+                }
+
+                try {
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
+                }
+            }
+            return super.onPreferenceTreeClick(preference);
     }
 }
