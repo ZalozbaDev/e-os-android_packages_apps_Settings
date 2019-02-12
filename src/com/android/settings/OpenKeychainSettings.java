2d14304d9fb22e79d92db0059bdc9b0008bca5fa
Romain Hunault <romain.hunault@e.email>
2019-02-12 14:40:22 +0000

    Settings: Add OpenKeychain in Settings
    Change-Id: If262f02d85f7fb81e5ae4eb7203ea06e1410748a
    Signed-off-by: Aayush Gupta <theimpulson@e.email>

---
+++ src/com/android/settings/OpenKeychainSettings.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2019-2021 ECORP SAS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.content.ComponentName;
+import androidx.fragment.app.Fragment;
+
+import android.app.Activity;
+import android.content.Context;
+import android.provider.SearchIndexableResource;
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.Indexable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OpenKeychainSettings extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent("android.intent.action.MAIN");
+        intent.setComponent(new ComponentName("org.sufficientlysecure.keychain", "org.sufficientlysecure.keychain.ui.MainActivity"));
+        startActivity(intent);
+
+        Activity activity = getActivity();
+
+        if (activity != null) {
+          activity.finish();
+        }
+    }
+
+    public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+    new BaseSearchIndexProvider() {
+        @Override
+        public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                boolean enabled) {
+            final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+
+            final SearchIndexableResource sir = new SearchIndexableResource(context);
+            sir.xmlResId = R.xml.open_keychain_prefs;
+            result.add(sir);
+            return result;
+        }
+    };
+}
