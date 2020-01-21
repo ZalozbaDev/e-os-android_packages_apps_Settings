2538d247d091f37c71ed27f0afbe5446d3537999
TChardeur <t.chardeur@e.email>
2020-01-21 17:12:23 +0100

    Test without backend

--- src/com/android/settings/accounts/AccountDetailDashboardFragment.java
+++ src/com/android/settings/accounts/AccountDetailDashboardFragment.java
@@ -54,7 +54,7 @@ public class AccountDetailDashboardFragment extends DashboardFragment {
     String mAccountType;
     private AccountSyncPreferenceController mAccountSynController;
     private RemoveAccountPreferenceController mRemoveAccountController;
-    private AccountRemoteStoragePreferenceController mAccountRemoteStorageController;
+    //private AccountRemoteStoragePreferenceController mAccountRemoteStorageController;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -78,7 +78,7 @@ public class AccountDetailDashboardFragment extends DashboardFragment {
         }
         mAccountSynController.init(mAccount, userHandle);
         mRemoveAccountController.init(mAccount, userHandle);
-        mAccountRemoteStorageController.init(mAccount, userHandle);
+        //mAccountRemoteStorageController.init(mAccount, userHandle);
     }
 
     @Override
