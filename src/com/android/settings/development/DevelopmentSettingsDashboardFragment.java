581758b8aa8576faf2eb935f399968e34ffc8900
Mohit Mali <futuretechnoworld.mohit@gmail.com>
2021-04-02 18:49:01 +0000

    Switch to select prod or staging OTA server

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2021 ECORP SAS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -456,6 +457,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
                 bluetoothA2dpConfigStore));
         controllers.add(new BluetoothMaxConnectedAudioDevicesPreferenceController(context));
         controllers.add(new ShowTapsPreferenceController(context));
+        controllers.add(new ChangeSourcePreferenceController(context));
         controllers.add(new PointerLocationPreferenceController(context));
         controllers.add(new ShowSurfaceUpdatesPreferenceController(context));
         controllers.add(new ShowLayoutBoundsPreferenceController(context));
