2802d23acc555e7ecf9eb53f06b479b37bb19518
Aayush Gupta <theimpulson@e.email>
2021-06-15 07:06:38 +0200

    Settings: Add missing copyright information
    Signed-off-by: Aayush Gupta <theimpulson@e.email>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1,5 +1,7 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2019-2021 E FOUNDATION
+ * Copyright (C) 2019-2021 ECORP SAS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
